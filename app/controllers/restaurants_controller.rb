f7794075cd1701bf6bf039ea1c48180aef8a0086
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-03-19 21:46:38 +0300

    Updated restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,7 +8,7 @@ class RestaurantsController < ApplicationController
       
       def show
           restaurant = find_restaurant
-          render json: restaurant, serializer: RestaurantWithPizzasSerializer        
+          render json: restaurant       
       end 
       
       def destroy
